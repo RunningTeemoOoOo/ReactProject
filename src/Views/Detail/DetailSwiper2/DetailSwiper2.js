e7d7057c8bb7a2fcf134f414a90a4b7b4f7a0a55
si1verZ <1052183744@qq.com>
2019-07-29 14:47:31 +0800

    0729lz

--- src/Views/Detail/DetailSwiper2/DetailSwiper2.js
+++ src/Views/Detail/DetailSwiper2/DetailSwiper2.js
@@ -4,7 +4,7 @@ import Swiper from 'swiper'
 import '_swiper@4.5.0@swiper/dist/css/swiper.css'
 import { withRouter } from 'react-router';
 
-class DetailSwiper extends Component {
+class DetailSwiper2 extends Component {
   render() {
     return(
       <div className={css.houses + ' modules'}>
@@ -12,18 +12,25 @@ class DetailSwiper extends Component {
           <p className={css.hotT}>精选房源</p>
           <p className={css.hotM}>更多></p>
         </div> */}
-        <div className={"swiper-info"}  style={{overflow: 'hidden'}}>
+        <div className={"swiper-container"}  style={{overflow: 'hidden'}}>
           <div className={"swiper-wrapper"} key={this.props.list.length}>
             {
               !this.props.list?null:
               this.props.list.map((item,index)=> {
                 return (
                   <div className={css.slide + " swiper-slide"} key={index}>
-                    <img src={item} alt="" />
+                    {/* <img src={item} alt="" /> */}
+                    <div className={css.box}>
+                    <div className={css.boxtitle}>{item.title}</div>
+                    <div className={css.boxsize}>总面积{item.size}平</div>
+                    <div className={css.boxprice} dangerouslySetInnerHTML={{ __html:item.price_rmb_string}}></div>
+                    </div>
+
                     {/* <p className={css.housetitl}> */}
                       {/* <span className={css.spanA}>{item.city_name}</span>
                       <span className={css.spanB}>{item.title}</span>
-                    </p> */}<div className={css.pagination +" swiper-pagination"}></div>
+                    </p> */}
+                    {/* <div className={css.pagination +" swiper-pagination"}></div> */}
                     {/* <p className={css.houseprice} dangerouslySetInnerHTML={{ __html: item.price_string}}></p> */}
                   </div>
                   
@@ -39,10 +46,10 @@ class DetailSwiper extends Component {
   }
   componentDidMount() {
     if (this.props.list) {
-      var swiper = new Swiper('.swiper-info', {
-        // slidesPerView: 1.2,
-        slidesPerView: 1,
-        // spaceBetween: 10,
+      var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1.6,
+        // slidesPerView: 2,
+        //  spaceBetween: 10,
         
         pagination: {
           clickable: true,
@@ -57,4 +64,4 @@ class DetailSwiper extends Component {
   }
 }
 
-export default withRouter(DetailSwiper)
+export default withRouter(DetailSwiper2)
