a7e9419a01e11333e23b58a82b91cc4f24661144
ljw <ljw970812@163.com>
2019-07-30 17:00:38 +0800

    emmmmm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import './App.css';
 
 class App extends Component {
   render() {
+    console.log('　　　　　　　 ┏┓       ┏┓+ +')
+    console.log('　　　　　　　┏┛┻━━━━━━━┛┻━━┓ + +')
+    console.log('　　　　　　　┃　　　　　　  ┃')
+    console.log('　　　　　　　┃　　　━　　　 ┃ ++ + + +')
+    console.log('　　　　　　 █████━█████    ┃+')
+    console.log('　　　　　　　┃　　　　　　  ┃ +')
+    console.log('　　　　　　　┃　　　┻　　 　┃')
+    console.log('　　　　　　　┃　　　　　 　 ┃ + +')
+    console.log('　　　　　　　┗━━━┓　　  ┏━━┛')
+    console.log('　　　　　　　　　┃　　  ┃')
+    console.log('　　　　　　　　　┃　　  ┃ + + + +')
+    console.log('　　　　　　　　　┃　　　┃　Code is far away from     bug with the animal protecting')
+    console.log('　　　　　　　　　┃　　　┃ + 　　　　         神兽保佑,代码无bug')
+    console.log('　　　　　　　　　┃　　　┃')
+    console.log('　　　　　　　　　┃　　　┃　　+')
+    console.log('　　　　　　　　　┃　 　 ┗━━━┓ + +')
+    console.log('　　　　　　　　　┃ 　　　 　 ┣┓')
+    console.log('　　　　　　　　　┃ 　　　　 ┏┛')
+    console.log('　　　　　　　　　┗┓┓┏━━━┳┓┏┛ + + + +')
+    console.log('　　　　　　　　　 ┃┫┫　 ┃┫┫')
+    console.log('　　　　　　　　　 ┗┻┛　 ┗┻┛+ + + +')
     return (
       <div style={{width: '100%', height: '100%'}}>
         {
